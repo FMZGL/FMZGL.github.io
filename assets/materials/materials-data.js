93906777d627e2038787975fcb77aa13b3e36fd4
FMZGL <yingguommlg@163.com>
2026-04-04 19:28:22 +0800

    Site updated: 2026-04-04 19:27:56

--- assets/materials/materials-data.js
+++ assets/materials/materials-data.js
@@ -39,5 +39,63 @@ window.MATERIALS_DATA = [
       '/assets/materials/files/其他实用文件/实验报告模板.doc',
       '/assets/materials/files/其他实用文件/人工智能相关名词.pdf'
     ]
+  },
+  {
+    id: '408专业课',
+    title: '数据结构',
+    files: [
+      '/assets/materials/files/数据结构/背诵知识点.pdf',
+      '/assets/materials/files/数据结构/大题.pdf',
+      '/assets/materials/files/数据结构/第1章 绪论.pdf',
+      '/assets/materials/files/数据结构/第2章 线性表.pdf',
+      '/assets/materials/files/数据结构/第3章 栈，队列和数组.pdf',
+      '/assets/materials/files/数据结构/第4章 串.pdf',
+      '/assets/materials/files/数据结构/第5章 树与二叉树.pdf',
+      '/assets/materials/files/数据结构/第6章 图.pdf',
+      '/assets/materials/files/数据结构/第7章 查找.pdf',
+      '/assets/materials/files/数据结构/第8章 排序.pdf',
+    ]
+  },
+  {
+    id: '408专业课',
+    title: '计算机组成原理',
+    files: [
+      '/assets/materials/files/计算机组成原理/背诵知识点.pdf',
+      '/assets/materials/files/计算机组成原理/大题.pdf',
+      '/assets/materials/files/计算机组成原理/第1章 计算机系统概述.pdf',
+      '/assets/materials/files/计算机组成原理/第2章 数据的表示和运算.pdf',
+      '/assets/materials/files/计算机组成原理/第3章 存储系统.pdf',
+      '/assets/materials/files/计算机组成原理/第4章 指令系统.pdf',
+      '/assets/materials/files/计算机组成原理/第5章 中央处理器.pdf',
+      '/assets/materials/files/计算机组成原理/第6章 总线.pdf',
+      '/assets/materials/files/计算机组成原理/第7章 输入输出系统.pdf',
+    ]
+  },
+  {
+    id: '408专业课',
+    title: '计算机网络',
+    files: [
+      '/assets/materials/files/计算机网络/背诵知识点.pdf',
+      '/assets/materials/files/计算机网络/大题.pdf',
+      '/assets/materials/files/计算机网络/第1章 计算机网络体系结构.pdf',
+      '/assets/materials/files/计算机网络/第2章 物理层.pdf',
+      '/assets/materials/files/计算机网络/第3章 数据链路层.pdf',
+      '/assets/materials/files/计算机网络/第4章 网络层.pdf',
+      '/assets/materials/files/计算机网络/第5章 传输层.pdf',
+      '/assets/materials/files/计算机网络/第6章 应用层.pdf',
+    ]
+  },
+  {
+    id: '408专业课',
+    title: '操作系统',
+    files: [
+      '/assets/materials/files/操作系统/背诵知识点.pdf',
+      '/assets/materials/files/操作系统/大题.pdf',
+      '/assets/materials/files/操作系统/第1章 计算机系统概述.pdf',
+      '/assets/materials/files/操作系统/第2章 进程与线程 .pdf',
+      '/assets/materials/files/操作系统/第3章 内存管理.pdf',
+      '/assets/materials/files/操作系统/第4章 文件管理.pdf',
+      '/assets/materials/files/操作系统/第5章 IO管理.pdf',
+    ]
   }
 ]
